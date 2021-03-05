5a5b02006c5b8e84d8f9abe69f315d08d36dd3dd
Al F <alexandreffaria@me.com>
2021-03-05 03:03:41 -0300

    testando programa indent

--- prog0301.c
+++ prog0301.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main(){
-	int x;
-	printf("Gimme a numb: ");
-	scanf("%d", &x);
-	if (x >= 0){
-		printf("Num Positivo.\n");
-	}
-	else{
-		printf("Num negativo.\n");
-	}
+int
+main ()
+{
+  int x;
+  printf ("Gimme a numb: ");
+  scanf ("%d", &x);
+  if (x >= 0)
+    {
+      printf ("Num Positivo.\n");
+    }
+  else
+    {
+      printf ("Num negativo.\n");
+    }
 
 }
